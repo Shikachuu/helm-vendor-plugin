3a592c784e2f177dbbd88f42b9aadcfdc18333b0
Czékus Máté <mate@picloud.hu>
2025-11-30 18:51:31 +0100

    feat: add helm plugin package distribution (#2)

---
+++ hack/helm-package.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+set -e
+
+PLUGIN_VERSION=$(yq eval '.version' plugin.yaml)
+
+mkdir -p "tmp-$OS-$ARCH/bin"
+cp "$BIN_PATH" "tmp-$OS-$ARCH/bin/$BIN_NAME"
+cp plugin.yaml "tmp-$OS-$ARCH/"
+
+mkdir -p "dist-$OS-$ARCH"
+
+echo "$HELM_KEY_PASSPHRASE" | helm plugin package \
+    --sign \
+    --key "$SIGNING_KEY_EMAIL" \
+    --keyring secring.gpg \
+    --passphrase-file - \
+    -d "dist-$OS-$ARCH" \
+    "tmp-$OS-$ARCH/"
+
+mkdir -p dist
+mv "dist-$OS-$ARCH/vendor-$PLUGIN_VERSION.tgz" "dist/vendor-$VERSION-$OS-$ARCH.tgz"
+mv "dist-$OS-$ARCH/vendor-$PLUGIN_VERSION.tgz.prov" "dist/vendor-$VERSION-$OS-$ARCH.tgz.prov"
+
+rm -rf "tmp-$OS-$ARCH" "dist-$OS-$ARCH"
+
